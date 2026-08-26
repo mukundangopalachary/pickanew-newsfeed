fc0805f2ffe989923e995c651a7134d19fb878e4
Mukundan Gopalachary <mukundangopalachary@gmail.com>
2026-08-26 18:27:46 +0530

    topics and articles model

---
+++ backend/application/src/main/java/app/news/backend/model/Article.java
@@ -0,0 +1,10 @@
+package app.news.backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "articles")
+public class Article {
+  
+}
---
+++ backend/application/src/main/java/app/news/backend/model/Topic.java
@@ -0,0 +1,28 @@
+package app.news.backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "topics")
+public class Topic {
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  
+}
